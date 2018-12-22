172edd17af5e121b3a251432b7e7a70e6da03e65
Dominique Mattern <dom.mattern@gmail.com>
2018-12-22 14:28:01 +0100

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'nestjs_admin',
+      password: 'nestjs_admin',
+      database: 'nestjs_typeorm_example',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
