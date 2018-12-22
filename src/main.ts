c06e505d85f85a7e4c4d80e63d5dbd1bfae49c5b
Dominique Mattern <dom.mattern@gmail.com>
2018-12-22 10:25:26 +0100

    Update space-before-function-paren rule

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,10 @@ CONFIG.load();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
+  const port = 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
