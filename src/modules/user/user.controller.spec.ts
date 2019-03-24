ab3aa2608f60d82a8809f778f33cd89cd47cbfe8
Dominique Mattern <dom.mattern@gmail.com>
2019-03-24 15:40:01 +0100

    Update user controller test

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,18 +1,56 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { ValueProvider } from '@nestjs/common/interfaces';
+import { Test } from '@nestjs/testing';
 
+import { USER_SERVICE } from './constants';
+import { IUserService } from './contracts';
+import { RegisterUserDto, UserDto } from './dto';
 import { UserController } from './user.controller';
 
 describe('User Controller', () => {
-  let module: TestingModule;
+  let userController: UserController;
+  let userService: IUserService;
 
-  beforeAll(async () => {
-    module = await Test.createTestingModule({
+  beforeEach(async () => {
+    userService = <IUserService> {};
+
+    const userServiceProvider: ValueProvider = {
+      provide: USER_SERVICE,
+      useValue: userService,
+    };
+
+    const module = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [userServiceProvider],
     }).compile();
+
+    userController = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
-    const controller: UserController = module.get<UserController>(UserController);
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  it('should sign up the user', async () => {
+    const userServiceSignUpReturnValue: UserDto = {
+      id: 'user-id',
+      email: 'john.doe@example.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'secret',
+      username: 'john.doe',
+      aboutMe: null,
+    };
+    userService.signUp = jest.fn().mockResolvedValue(userServiceSignUpReturnValue);
+
+    const userToRegister: RegisterUserDto = {
+      email: 'john.doe@example.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'secret',
+      username: 'john.doe',
+    };
+    const registeredUser = await userController.signUp(userToRegister);
+
+    expect(registeredUser).toEqual(userServiceSignUpReturnValue);
   });
 });
